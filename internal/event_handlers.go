b43dabc0113faf04294028a3a47aed6fc7f2468c
Petro Svintsitskyi <psvintsitskyi@rebbix.com>
2020-02-17 11:05:56 +0200

    avoid race conditions

--- internal/event_handlers.go
+++ internal/event_handlers.go
@@ -6,9 +6,11 @@ import (
 	"github.com/peter-svintsitskyi/go-gesclient/client"
 	"github.com/peter-svintsitskyi/go-gesclient/log"
 	"reflect"
+	"sync"
 )
 
 type eventHandlers struct {
+	mu sync.Mutex
 	handlers []client.EventHandler
 }
 
@@ -19,11 +21,15 @@ func newEventHandlers() *eventHandlers {
 }
 
 func (h *eventHandlers) Add(handler client.EventHandler) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	h.handlers = append(h.handlers, handler)
 	return nil
 }
 
 func (h *eventHandlers) Remove(handler client.EventHandler) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 	pos := -1
 	for i, h := range h.handlers {
 		if fmt.Sprintf("%v", h) == fmt.Sprintf("%v", handler) {
